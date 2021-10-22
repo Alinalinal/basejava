089a64a8b8ca918aa329fc16be9b9b962cadd8f0
Alina Bogoslavets <alinalinal@gmail.com>
2021-10-22 17:15:39 +0300

    Add variable 'size', rewrite methods in ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,58 +3,42 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
-        int size = size();
         for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
+        size = 0;
     }
 
     void save(Resume r) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                storage[i] = r;
-                break;
-            } else if (storage[i].uuid.equals(r.uuid)) {
-                break;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(r.uuid)) {
+                // only unique Resumes are stored in the storage
+                return;
             }
         }
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-        for (Resume resume : storage) {
-            if (resume == null) {
-                // no Resume with this uuid
-                break;
-            } else if (resume.uuid.equals(uuid)) {
-                return resume;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
             }
         }
         return null;
     }
 
     void delete(String uuid) {
-        int size = size();
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                storage[i] = null;
-                rebuiltArray();
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
                 break;
-            }
-        }
-    }
-
-    /**
-     * nulls to the tail of a storage
-     */
-    private void rebuiltArray() {
-        for (int i = 0; i < storage.length - 1; i++) {
-            if (storage[i] == null && storage[i + 1] == null) {
-                break;
-            } else if (storage[i] == null && storage[i + 1] != null) {
-                storage[i] = storage[i + 1];
-                storage[i + 1] = null;
             }
         }
     }
@@ -63,21 +47,12 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int size = size();
         Resume[] arr = new Resume[size];
         System.arraycopy(storage, 0, arr, 0, size);
         return arr;
     }
 
     int size() {
-        int size = 0;
-        for (Resume resume : storage) {
-            if (resume == null) {
-                return size;
-            } else {
-                size++;
-            }
-        }
         return size;
     }
 }
