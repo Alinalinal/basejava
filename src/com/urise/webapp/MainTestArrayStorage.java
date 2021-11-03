ab06094629c8e5199fe4db11ca732615025ca260
Alina Bogoslavets <alinalinal@gmail.com>
2021-11-03 21:48:35 +0300

    Changes in ArrayStorage.java & MainTestArrayStorage.java

--- src/com/urise/webapp/MainTestArrayStorage.java
+++ src/com/urise/webapp/MainTestArrayStorage.java
@@ -21,18 +21,50 @@ public class MainTestArrayStorage {
         ARRAY_STORAGE.save(r2);
         ARRAY_STORAGE.save(r3);
 
+        ARRAY_STORAGE.save(r3);
+        System.out.println();
+
         System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
         System.out.println("Size: " + ARRAY_STORAGE.size());
+        System.out.println();
 
         System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
+        System.out.println();
+
+        Resume r4 = new Resume();
+        r4.setUuid("uuid2");
+        ARRAY_STORAGE.update(r4);
+        r4.setUuid("uuid222");
+        System.out.println("Get r4: " + ARRAY_STORAGE.get(r4.getUuid()));
+        System.out.println();
+
+        Resume r5 = new Resume();
+        r5.setUuid("uuid5");
+        ARRAY_STORAGE.update(r5);
+        ARRAY_STORAGE.delete(r5.getUuid());
 
         printAll();
         ARRAY_STORAGE.delete(r1.getUuid());
         printAll();
         ARRAY_STORAGE.clear();
         printAll();
+        System.out.println();
 
         System.out.println("Size: " + ARRAY_STORAGE.size());
+        System.out.println();
+
+        ARRAY_STORAGE.save(r1);
+        System.out.println(ARRAY_STORAGE.size());
+        ARRAY_STORAGE.clear();
+        System.out.println();
+
+        Resume resume;
+        for (int i = 0; i <= 10000; i++) {
+            resume = new Resume();
+            resume.setUuid("uuid" + (i + 1));
+            ARRAY_STORAGE.save(resume);
+        }
+        System.out.println(ARRAY_STORAGE.size());
     }
 
     static void printAll() {
--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -2,33 +2,43 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+    private final Resume[] storage = new Resume[10000];
     private int size = 0;
 
     public void clear() {
-        for (int i = 0; i < size; i++) {
-            storage[i] = null;
-        }
+        Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
     public void update(Resume resume) {
-
-    }
-
-    public void save(Resume resume) {
         for (int i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(resume.getUuid())) {
-                // only unique Resumes are stored in the storage
+                storage[i] = resume;
                 return;
             }
         }
-        storage[size] = resume;
-        size++;
+        printIsPresentError(false, resume.getUuid());
+    }
+
+    public void save(Resume resume) {
+        if (size == storage.length) {
+            System.out.println("ERROR: Storage is full, you cannot save resume with 'uuid = " + resume.getUuid() + "'!");
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (storage[i].getUuid().equals(resume.getUuid())) {
+                    printIsPresentError(true, resume.getUuid());
+                    return;
+                }
+            }
+            storage[size] = resume;
+            size++;
+        }
     }
 
     public Resume get(String uuid) {
@@ -37,6 +47,7 @@ public class ArrayStorage {
                 return storage[i];
             }
         }
+        printIsPresentError(false, uuid);
         return null;
     }
 
@@ -46,21 +57,25 @@ public class ArrayStorage {
                 storage[i] = storage[size - 1];
                 storage[size - 1] = null;
                 size--;
-                break;
+                return;
             }
         }
+        printIsPresentError(false, uuid);
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-        Resume[] result = new Resume[size];
-        System.arraycopy(storage, 0, result, 0, size);
-        return result;
+        return Arrays.copyOf(storage, size);
     }
 
     public int size() {
         return size;
+    }
+
+    private void printIsPresentError(boolean isPresent, String uuid) {
+        System.out.println(isPresent ? "ERROR: Resume with 'uuid = " + uuid + "' already exists in storage!"
+                : "ERROR: No resume with 'uuid = " + uuid + "' in storage!");
     }
 }
