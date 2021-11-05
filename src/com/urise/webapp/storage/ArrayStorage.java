e4aee9c1b104593489c048a1719a66aec0c5db91
Alina Bogoslavets <alinalinal@gmail.com>
2021-11-05 19:47:37 +0300

    L3: Changed Resume.java & ArrayStorage.java
    - Add equals() & hashcode() in Resume.java
    - Add static variable STORAGE_LIMIT in ArrayStorage.java

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -8,7 +8,9 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private final Resume[] storage = new Resume[10000];
+    private static final int STORAGE_LIMIT = 10000;
+
+    private final Resume[] storage = new Resume[STORAGE_LIMIT];
     private int size = 0;
 
     public void clear() {
@@ -26,7 +28,7 @@ public class ArrayStorage {
     }
 
     public void save(Resume resume) {
-        if (size == storage.length) {
+        if (size == STORAGE_LIMIT) {
             System.out.println("ERROR: Storage is full, you cannot save resume with 'uuid = " + resume.getUuid() + "'!");
         } else if (findIndex(resume.getUuid()) != -1) {
             printIsPresentError(true, resume.getUuid());
