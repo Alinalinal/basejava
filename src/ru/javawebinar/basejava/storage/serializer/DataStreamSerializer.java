0ac785d708688493294adac37437057172e8a538
Alina Bogoslavets <alinalinal@gmail.com>
2022-02-04 13:58:17 +0300

    L10: HW10
    - Refactor

--- src/ru/javawebinar/basejava/storage/serializer/DataStreamSerializer.java
+++ src/ru/javawebinar/basejava/storage/serializer/DataStreamSerializer.java
@@ -14,110 +14,43 @@ public class DataStreamSerializer implements StreamSerializer {
         try (DataOutputStream dos = new DataOutputStream(os)) {
             dos.writeUTF(resume.getUuid());
             dos.writeUTF(resume.getFullName());
-            Map<ContactType, String> contacts = resume.getContacts();
-            dos.writeInt(contacts.size());
-            WriteDataConsumer.writeWithException(contacts.entrySet(), dos, (dos1, o) -> {
-                Map.Entry<ContactType, String> entry = (Map.Entry<ContactType, String>) o;
-                dos1.writeUTF(entry.getKey().name());
-                dos1.writeUTF(entry.getValue());
+            WriteDataConsumer.writeWithException(resume.getContacts().entrySet(), dos, entry -> {
+                dos.writeUTF(entry.getKey().name());
+                dos.writeUTF(entry.getValue());
             });
-            Map<SectionType, AbstractSection> sections = resume.getSections();
-            dos.writeInt(sections.size());
-            WriteDataConsumer.writeWithException(sections.entrySet(), dos, (dos12, o) -> {
-                Map.Entry<SectionType, AbstractSection> entry = (Map.Entry<SectionType, AbstractSection>) o;
+            WriteDataConsumer.writeWithException(resume.getSections().entrySet(), dos, entry -> {
                 SectionType sectionType = entry.getKey();
-                dos12.writeUTF(sectionType.name());
+                dos.writeUTF(sectionType.name());
                 AbstractSection section = entry.getValue();
                 switch (sectionType) {
                     case OBJECTIVE:
                     case PERSONAL:
-                        dos12.writeUTF(((TextSection)section).getContent());
+                        dos.writeUTF(((TextSection) section).getContent());
                         break;
                     case ACHIEVEMENT:
                     case QUALIFICATIONS:
-                        List<String> list =
-                                ((ListSection) entry.getValue()).getContent();
-                        dos12.writeInt(list.size());
-                        WriteDataConsumer.writeWithException(list, dos, (dos13, o1) -> dos13.writeUTF((String) o1));
+                        WriteDataConsumer.writeWithException(((ListSection) section).getContent(), dos,
+                                dos::writeUTF);
                         break;
                     case EXPERIENCE:
                     case EDUCATION:
-                        List<Organization> content = ((OrganizationSection)
-                                entry.getValue()).getContent();
-                        dos12.writeInt(content.size());
-                        WriteDataConsumer.writeWithException(content, dos, (dos15, o13) -> {
-                            Organization organization = (Organization) o13;
-                            Link link = organization.getHomePage();
-                            dos12.writeUTF(link.getName());
-                            String url = link.getUrl();
-                            dos12.writeUTF(url == null ? "null" : url);
-                            List<Organization.Position> positions = organization.getPositions();
-                            dos12.writeInt(positions.size());
-                            WriteDataConsumer.writeWithException(positions, dos15, (dos14, o12) -> {
-                                Organization.Position position = (Organization.Position) o12;
-                                DateUtil.writeAsData(dos12, position.getStartDate());
-                                DateUtil.writeAsData(dos12, position.getEndDate());
-                                dos12.writeUTF(position.getTitle());
-                                String description = position.getDescription();
-                                dos12.writeUTF(description == null ? "null" : description);
-                            });
-                        });
+                        WriteDataConsumer.writeWithException(((OrganizationSection) section).getContent(), dos,
+                                organization -> {
+                                    Link link = organization.getHomePage();
+                                    dos.writeUTF(link.getName());
+                                    String url = link.getUrl();
+                                    dos.writeUTF(url == null ? "null" : url);
+                                    WriteDataConsumer.writeWithException(organization.getPositions(), dos, position -> {
+                                        DateUtil.writeAsData(dos, position.getStartDate());
+                                        DateUtil.writeAsData(dos, position.getEndDate());
+                                        dos.writeUTF(position.getTitle());
+                                        String description = position.getDescription();
+                                        dos.writeUTF(description == null ? "null" : description);
+                                    });
+                                });
                         break;
                 }
             });
-
-            /*
-            dos.writeUTF(resume.getUuid());
-            dos.writeUTF(resume.getFullName());
-            Map<ContactType, String> contacts = resume.getContacts();
-            dos.writeInt(contacts.size());
-            /*for (Map.Entry<ContactType, String> entry : contacts.entrySet()) {
-                dos.writeUTF(entry.getKey().name());
-                dos.writeUTF(entry.getValue());
-            }*/
-            /*contacts.entrySet().forEach((entry) -> {
-                dos.writeUTF(entry.getKey().name());
-                dos.writeUTF(String.valueOf(entry.getValue()));
-            });
-            //Set<Map.Entry<ContactType, String>> set = contacts.entrySet();
-            for (Map.Entry<SectionType, AbstractSection> entry : sections.entrySet()) {
-                SectionType sectionType = entry.getKey();
-                dos.writeUTF(sectionType.name());
-                switch (sectionType) {
-                    case OBJECTIVE:
-                    case PERSONAL:
-                        dos.writeUTF(((TextSection) entry.getValue()).getContent());
-                        break;
-                    case ACHIEVEMENT:
-                    case QUALIFICATIONS:
-                        List<String> list = ((ListSection) entry.getValue()).getContent();
-                        dos.writeInt(list.size());
-                        for (String s : list) {
-                            dos.writeUTF(s);
-                        }
-                        break;
-                    case EXPERIENCE:
-                    case EDUCATION:
-                        List<Organization> content = ((OrganizationSection) entry.getValue()).getContent();
-                        dos.writeInt(content.size());
-                        for (Organization org : content) {
-                            Link link = org.getHomePage();
-                            dos.writeUTF(link.getName());
-                            String url = link.getUrl();
-                            dos.writeUTF(url == null ? "null" : url);
-                            List<Organization.Position> positions = org.getPositions();
-                            dos.writeInt(positions.size());
-                            for (Organization.Position position : positions) {
-                                DateUtil.writeAsData(dos, position.getStartDate());
-                                DateUtil.writeAsData(dos, position.getEndDate());
-                                dos.writeUTF(position.getTitle());
-                                String description = position.getDescription();
-                                dos.writeUTF(description == null ? "null" : description);
-                            }
-                        }
-                        break;
-                }
-            } */
         }
     }
 
@@ -175,12 +108,13 @@ public class DataStreamSerializer implements StreamSerializer {
 
     @FunctionalInterface
     public interface WriteDataConsumer<T> {
-        void writeData(DataOutputStream dos, T t) throws IOException;
+        void writeData(T t) throws IOException;
 
-        static <T> void writeWithException(Collection<T> collection, DataOutputStream dos, WriteDataConsumer consumer)
-                throws IOException {
-            for (T t: collection) {
-                consumer.writeData(dos, t);
+        static <T> void writeWithException(Collection<T> collection, DataOutputStream dos,
+                                           WriteDataConsumer<T> consumer) throws IOException {
+            dos.writeInt(collection.size());
+            for (T t : collection) {
+                consumer.writeData(t);
             }
         }
     }
