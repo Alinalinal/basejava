5e37554dc0ce359376eb499209e405f0dd4f9d62
Alina Bogoslavets <alinalinal@gmail.com>
2022-02-15 15:36:27 +0300

    L11: HW11

--- src/ru/javawebinar/basejava/MainDeadlock.java
+++ src/ru/javawebinar/basejava/MainDeadlock.java
@@ -1,29 +1,35 @@
 package ru.javawebinar.basejava;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class MainDeadlock {
 
-    private static final Object LOCK_1 = new Object();
-    private static final Object LOCK_2 = new Object();
+    private static final Lock LOCK_1 = new ReentrantLock();
+    private static final Lock LOCK_2 = new ReentrantLock();
 
     public static void main(String[] args) {
-        Thread thread = new Thread(() -> {
-            doDeadlock(LOCK_1, LOCK_2);
-        });
-        thread.start();
-
-        doDeadlock(LOCK_2, LOCK_1);
+        startThread(LOCK_1, LOCK_2);
+        startThread(LOCK_2, LOCK_1);
     }
 
-    private static void doDeadlock(Object lock1, Object lock2) {
-        synchronized (lock1) {
+    private static void startThread(Lock lock1, Lock lock2) {
+        new Thread(() -> {
+            tryLock(lock1);
             try {
                 Thread.sleep(5000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            synchronized (lock2) {
-                System.out.println("Success!");
-            }
-        }
+            tryLock(lock2);
+            lock2.unlock();
+            lock1.unlock();
+        }).start();
+    }
+
+    private static void tryLock(Lock lock) {
+        System.out.println(Thread.currentThread().getName() + " trying to get lock on " + lock);
+        lock.lock();
+        System.out.println(Thread.currentThread().getName() + " get lock on " + lock);
     }
 }
