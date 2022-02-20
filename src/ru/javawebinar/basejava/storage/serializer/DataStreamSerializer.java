cbac60ceba1d4d5ab73763cf6e9b92eec27b72cc
Alina Bogoslavets <alinalinal@gmail.com>
2022-02-20 14:20:08 +0300

    L12: HW10

--- src/ru/javawebinar/basejava/storage/serializer/DataStreamSerializer.java
+++ src/ru/javawebinar/basejava/storage/serializer/DataStreamSerializer.java
@@ -4,7 +4,6 @@ import ru.javawebinar.basejava.model.*;
 import ru.javawebinar.basejava.util.DateUtil;
 
 import java.io.*;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -15,11 +14,11 @@ public class DataStreamSerializer implements StreamSerializer {
         try (DataOutputStream dos = new DataOutputStream(os)) {
             dos.writeUTF(resume.getUuid());
             dos.writeUTF(resume.getFullName());
-            writeWithException(resume.getContacts().entrySet(), dos, entry -> {
+            writeCollection(resume.getContacts().entrySet(), dos, entry -> {
                 dos.writeUTF(entry.getKey().name());
                 dos.writeUTF(entry.getValue());
             });
-            writeWithException(resume.getSections().entrySet(), dos, entry -> {
+            writeCollection(resume.getSections().entrySet(), dos, entry -> {
                 SectionType sectionType = entry.getKey();
                 dos.writeUTF(sectionType.name());
                 AbstractSection section = entry.getValue();
@@ -30,18 +29,18 @@ public class DataStreamSerializer implements StreamSerializer {
                         break;
                     case ACHIEVEMENT:
                     case QUALIFICATIONS:
-                        writeWithException(((ListSection) section).getContent(), dos,
+                        writeCollection(((ListSection) section).getContent(), dos,
                                 dos::writeUTF);
                         break;
                     case EXPERIENCE:
                     case EDUCATION:
-                        writeWithException(((OrganizationSection) section).getContent(), dos,
+                        writeCollection(((OrganizationSection) section).getContent(), dos,
                                 organization -> {
                                     Link link = organization.getHomePage();
                                     dos.writeUTF(link.getName());
                                     String url = link.getUrl();
                                     dos.writeUTF(url == null ? "null" : url);
-                                    writeWithException(organization.getPositions(), dos, position -> {
+                                    writeCollection(organization.getPositions(), dos, position -> {
                                         DateUtil.writeAsData(dos, position.getStartDate());
                                         DateUtil.writeAsData(dos, position.getEndDate());
                                         dos.writeUTF(position.getTitle());
@@ -69,48 +68,18 @@ public class DataStreamSerializer implements StreamSerializer {
                         break;
                     case ACHIEVEMENT:
                     case QUALIFICATIONS:
-                        List<String> content = new ArrayList<>();
-                        readWithException(dis, () -> content.add(dis.readUTF()));
-                        resume.addSection(sectionType, new ListSection(content));
+                        resume.addSection(sectionType, new ListSection(readCollection(dis, dis::readUTF)));
                         break;
                     case EXPERIENCE:
                     case EDUCATION:
-                        List<Organization> orgContent = new ArrayList<>();
-                        readWithException(dis, () -> {
-                            String name = dis.readUTF();
-                            String url = dis.readUTF();
-                            Link link = new Link(name, (url.equals("null") ? null : url));
-                            List<Organization.Position> posContent = new ArrayList<>();
-                            readWithException(dis, () -> {
-                                LocalDate startDate = DateUtil.readDataDate(dis);
-                                LocalDate endDate = DateUtil.readDataDate(dis);
-                                String title = dis.readUTF();
-                                String description = dis.readUTF();
-                                posContent.add(new Organization.Position(startDate, endDate, title,
-                                        (description.equals("null") ? null : description)));
-                            });
-                            orgContent.add(new Organization(link, posContent));
-                        });
-                        resume.addSection(sectionType, new OrganizationSection(orgContent));
+                        resume.addSection(sectionType, new OrganizationSection(readCollection(dis, () ->
+                                new Organization(new Link(dis.readUTF(), dis.readUTF()), readCollection(dis, () ->
+                                        new Organization.Position(DateUtil.readDataDate(dis),
+                                                DateUtil.readDataDate(dis), dis.readUTF(), dis.readUTF()))))));
                         break;
                 }
             });
             return resume;
-        }
-    }
-
-    private static <T> void writeWithException(Collection<T> collection, DataOutputStream dos,
-                                               WriteDataConsumer<T> consumer) throws IOException {
-        dos.writeInt(collection.size());
-        for (T t : collection) {
-            consumer.writeData(t);
-        }
-    }
-
-    private static void readWithException(DataInputStream dis, ReadDataConsumer consumer) throws IOException {
-        int size = dis.readInt();
-        for (int i = 0; i < size; i++) {
-            consumer.readData();
         }
     }
 
@@ -119,8 +88,37 @@ public class DataStreamSerializer implements StreamSerializer {
         void writeData(T t) throws IOException;
     }
 
+    private static <T> void writeCollection(Collection<T> collection, DataOutputStream dos,
+                                            WriteDataConsumer<T> consumer) throws IOException {
+        dos.writeInt(collection.size());
+        for (T t : collection) {
+            consumer.writeData(t);
+        }
+    }
+
+    @FunctionalInterface
+    private interface ElementReader<T> {
+        T readDataElement() throws IOException;
+    }
+
+    private static <T> List<T> readCollection(DataInputStream dis, ElementReader<T> elementReader) throws IOException {
+        int size = dis.readInt();
+        List<T> list = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            list.add(elementReader.readDataElement());
+        }
+        return list;
+    }
+
     @FunctionalInterface
     private interface ReadDataConsumer {
         void readData() throws IOException;
+    }
+
+    private static void readWithException(DataInputStream dis, ReadDataConsumer consumer) throws IOException {
+        int size = dis.readInt();
+        for (int i = 0; i < size; i++) {
+            consumer.readData();
+        }
     }
 }
