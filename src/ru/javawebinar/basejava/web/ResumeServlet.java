8eceb65d18b834c23de5b2e14dd2f4e19b2819d4
Alina Bogoslavets <alinalinal@gmail.com>
2022-10-13 18:09:54 +0300

    L16: HW15 Servlet
    - Changes in: - ResumeServlet.java
                  - SqlStorage.java
                  - Config.java
                  - test.html

--- src/ru/javawebinar/basejava/web/ResumeServlet.java
+++ src/ru/javawebinar/basejava/web/ResumeServlet.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.basejava.web;
 
 import ru.javawebinar.basejava.Config;
+import ru.javawebinar.basejava.model.ContactType;
 import ru.javawebinar.basejava.model.Resume;
 import ru.javawebinar.basejava.storage.Storage;
 
@@ -9,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
+import java.io.Writer;
 
 public class ResumeServlet extends HttpServlet {
     private Storage storage;
@@ -17,9 +18,7 @@ public class ResumeServlet extends HttpServlet {
     @Override
     public void init() throws ServletException {
         super.init();
-        System.out.println(storage);
         storage = Config.get().getSqlStorage();
-        System.out.println(storage);
     }
 
     @Override
@@ -31,44 +30,33 @@ public class ResumeServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
-        response.setHeader("Content-Type", "text/html; charset=UTF-8");
+        // response.setHeader("Content-Type", "text/html; charset=UTF-8");
         response.setContentType("text/html; charset=UTF-8");
 
-        StringBuilder resumeTable = new StringBuilder();
-        resumeTable.append("<!DOCTYPE html>\n" +
+        Writer writer = response.getWriter();
+        writer.write("<!DOCTYPE html>\n" +
                 "<html lang=\"en\">\n" +
                 "<head>\n" +
-                "    <title>Resume</title>\n" +
                 "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/>\n" +
-                "    <style>\n" +
-                "        table, th, td {\n" +
-                "            border: 1px solid black;\n" +
-                "            border-collapse: collapse;\n" +
-                "            margin: auto;\n" +
-                "        }\n" +
-                "        td {\n" +
-                "            text-align: center;\n" +
-                "        }\n" +
-                "    </style>\n" +
+                "    <link rel=\"stylesheet\" href=\"css/style.css\">\n" +
+                "    <title>Список всех резюме</title>\n" +
                 "</head>\n" +
                 "<body>\n" +
-                "<table class=\"resume-table\" style=\"width:500px\">\n" +
-                "    <caption><h4>Resume table</h4></caption>\n" +
+                "<section>\n" +
+                "<table border=\"1\" cellpadding=\"8\" cellspacing=\"0\">\n" +
                 "    <tr style=\"height:40px\">\n" +
-                "        <th>UUID</th>\n" +
-                "        <th>FULL NAME</th>\n" +
+                "        <th>Имя</th>\n" +
+                "        <th>Email</th>\n" +
                 "    </tr>\n");
-        List<Resume> resumes = storage.getAllSorted();
-        for (Resume r : resumes) {
-            resumeTable.append("<tr style=\"height:40px\">\n" +
-                    "        <td>" + r.getUuid() + "</td>\n" +
-                    "        <td>" + r.getFullName() + "</td>\n" +
+        for (Resume resume : storage.getAllSorted()) {
+            writer.write("<tr>\n" +
+                    "        <td><a href=\"resume?uuid=" + resume.getUuid() + "\">" + resume.getFullName() + "</a></td>\n" +
+                    "        <td>" + resume.getContact(ContactType.EMAIL) + "</td>\n" +
                     "    </tr>\n");
         }
-        resumeTable.append("</table>\n" +
+        writer.write("</table>\n" +
+                "</section>\n" +
                 "</body>\n" +
                 "</html>");
-
-        response.getWriter().write(String.valueOf(resumeTable));
     }
 }
