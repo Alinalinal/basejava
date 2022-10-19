c2948f095243ad64a1980330a20e80e2672323be
Alina Bogoslavets <alinalinal@gmail.com>
2022-10-19 23:27:44 +0300

    L16: crud
    - Changes in: - list.jsp
                  - ContactType.java
                  - Resume.java
                  - ResumeServlet.java
                  - style.css
                  - index.html
    - Add: - web/img directory
           - add.png, delete.png, email.png, gh.png, lin.png, pencil.png, skype.png, so.png in img
           - edit.jsp & view.jsp in WEB-INF/jsp

--- src/ru/javawebinar/basejava/model/ContactType.java
+++ src/ru/javawebinar/basejava/model/ContactType.java
@@ -4,14 +4,44 @@ public enum ContactType {
     PHONE_NUMBER("Тел.:"),
     MOBILE_PHONE_NUMBER("Мобильный:"),
     HOME_PHONE_NUMBER("Домашний тел.:"),
-    SKYPE("Skype:"),
-    EMAIL("Почта:"),
-    LINKED_IN("Профиль LinkedIn"),
-    GIT_HUB("Профиль GitHub"),
-    STACKOVERFLOW("Профиль Stackoverflow"),
-    HOME_PAGE("Домашняя страница");
+    SKYPE("Skype:") {
+        @Override
+        public String toHtml0(String value) {
+            return getTitle() + ": " + toLink("skype:" + value, value);
+        }
+    },
+    EMAIL("Почта:") {
+        @Override
+        public String toHtml0(String value) {
+            return getTitle() + ": " + toLink("mailto:" + value, value);
+        }
+    },
+    LINKED_IN("Профиль LinkedIn") {
+        @Override
+        public String toHtml0(String value) {
+            return toLink(value);
+        }
+    },
+    GIT_HUB("Профиль GitHub") {
+        @Override
+        public String toHtml0(String value) {
+            return toLink(value);
+        }
+    },
+    STACKOVERFLOW("Профиль Stackoverflow") {
+        @Override
+        public String toHtml0(String value) {
+            return toLink(value);
+        }
+    },
+    HOME_PAGE("Домашняя страница") {
+        @Override
+        public String toHtml0(String value) {
+            return toLink(value);
+        }
+    };
 
-    private String title;
+    private final String title;
 
     ContactType(String title) {
         this.title = title;
@@ -19,5 +49,21 @@ public enum ContactType {
 
     public String getTitle() {
         return title;
+    }
+
+    protected String toHtml0(String value) {
+        return title + ": " + value;
+    }
+
+    public String toHtml(String value) {
+        return (value == null) ? "" : toHtml0(value);
+    }
+
+    public String toLink(String href) {
+        return toLink(href, title);
+    }
+
+    public static String toLink(String href, String title) {
+        return "<a href='" + href + "'>" + title + "</a>";
     }
 }
